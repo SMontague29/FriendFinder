79fa2160c6ca94be66749486413a4e49a8db286d
Scott Montague <SMontague29@gmail.com>
2017-08-13 15:36:44 -0600

    Add files via upload

---
+++ app/data/friends.js
@@ -0,0 +1,52 @@
+var userData = [
+  {
+    "name": "Curly",
+    "photo": "https://avatars0.githubusercontent.com/u/20157409?v=4&u=6e0622cccdd83b8da22589585a470ed2c3847006&s=400",
+    "scores":[
+        5,
+      	1,
+      	4,
+      	4,
+      	5,
+      	1,
+      	2,
+      	5,
+      	4,
+      	1
+    ]
+  },
+  {
+    "name": "Moe",
+    "photo": "https://avatars0.githubusercontent.com/u/20157409?v=4&u=6e0622cccdd83b8da22589585a470ed2c3847006&s=400",
+    "scores":[
+        3,
+        2,
+        2,
+        4,
+        1,
+        1,
+        5,
+        1,
+        3,
+        1
+    ]
+  },
+  {
+    "name": "Olive",
+    "photo": "https://avatars0.githubusercontent.com/u/20157409?v=4&u=6e0622cccdd83b8da22589585a470ed2c3847006&s=400",
+    "scores":[
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1
+    ]
+  }
+];
+
+module.exports = userData;
